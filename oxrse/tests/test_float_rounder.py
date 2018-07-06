d4ab2b760f583274baf2f1a38e800e3560c3e175
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2018-07-06 14:10:37 +0100

    Added test for round-towards-zero.

--- oxrse/tests/test_float_rounder.py
+++ oxrse/tests/test_float_rounder.py
@@ -48,3 +48,35 @@ class FloatRounderTest(unittest.TestCase):
 
         # Test special values raise the appropriate exception
         self.assertRaisesRegexp(ValueError, 'worst number', r.round, 2.34)
+
+    def test_round_towards_zero(self):
+        """
+        Tests :meth:`FloatRounder.round()` in round-towards-zero mode.
+        """
+        r = oxrse.FloatRounder(True)
+
+        # Test with positive numbers
+        self.assertEqual(r.round(0), 0.0)
+        self.assertEqual(r.round(0.4), 0.0)
+        self.assertEqual(r.round(0.5), 0.0)
+        self.assertEqual(r.round(0.5001), 0.0)
+        self.assertEqual(r.round(0.9), 0.0)
+        self.assertEqual(r.round(1.49), 1.0)
+        self.assertEqual(r.round(1000.5), 1000.0)
+        self.assertEqual(r.round(1000.5001), 1000.0)
+
+        # Test with negative numbers
+        self.assertEqual(r.round(-0), 0.0)
+        self.assertEqual(r.round(-0.4), 0.0)
+        self.assertEqual(r.round(-0.5), 0.0)
+        self.assertEqual(r.round(-0.5001), 0.0)
+        self.assertEqual(r.round(-0.9), 0.0)
+        self.assertEqual(r.round(-1.49), -1.0)
+        self.assertEqual(r.round(-1000.5), -1000.0)
+        self.assertEqual(r.round(-1000.5001), -1000.0)
+
+        # Test special values raise the appropriate exception
+        self.assertRaisesRegexp(ValueError, "don't like", r.round, 1.23)
+
+        # Test special values raise the appropriate exception
+        self.assertRaisesRegexp(ValueError, 'worst number', r.round, 2.34)
